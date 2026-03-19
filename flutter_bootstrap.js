ac5c4e1fd84c59140b5aa1b3ab7f49aec5aa983a
radiumwater256 <radiumwater256@remote>
2026-03-19 11:12:04 +0800

    UI improvement

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "859599081"
+    serviceWorkerVersion: "3690704208"
   }
 });
