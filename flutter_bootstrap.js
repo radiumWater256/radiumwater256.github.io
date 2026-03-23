f0312c66ff522bc1d60436bfc9694c9b812cb5af
radiumwater256 <radiumwater256@remote>
2026-03-23 21:34:30 +0800

    Attempt to fix small-screen UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1021868393"
+    serviceWorkerVersion: "3087104784"
   }
 });
