2d876bdf2e679562945f784dd3f8c5986103cf13
radiumwater256 <radiumwater256@remote>
2026-07-06 19:16:29 +0800

    Added Missing return button in traffic news

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2416678347" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2502154146" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
